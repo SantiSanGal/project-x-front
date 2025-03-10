b6b25f69457ee2032f524695f29be4c04d194cc7
SantiagoGalvan <santiagon.g.m@gmail.com>
2025-03-09 23:11:00 -0300

    chore: improve about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,37 +1,34 @@
 export const About = () => {
   return (
     <div className="w-screen min-h-screen flex items-center justify-center bg-stone-900">
-      <div className="flex flex-col justify-center text-slate-300 p-4 rounded-md bg-stone-800 shadow-lg text-aliceblue max-w-[500px] min-w-[400px] min-h-[200px] w-[40vw]">
+      <div className="flex flex-col justify-center text-slate-300 p-4 rounded-md bg-stone-800 shadow-lg text-aliceblue w-[95vw] xl:max-w-[60vw]">
         <h2 className="text-xl font-semibold text-white mb-4">
-          About <span className="text-lime-600">Pixel</span>
+          About <span className="text-lime-600">PixelsWar & Me</span>
         </h2>
 
         <p className="mb-2">
-          Pixel started as an idea to support my family and make our dreams come true—an idea that began 20 years ago.
+          PixelsWar started as an idea to support my family and make our dreams come true—an idea that began 20 years ago.
+        </p>
+
+        <p className="mb-2">
+          PixelsWar is inspired by the iconic <span className="text-lime-600">Million Dollar Homepage</span> created by Alex Tew in 2005, a groundbreaking project that sold pixels as advertising space. At the time, I was only three years old and couldn't compete with him, but now, with modern technology, I’m bringing the concept back with a twist. My version enhances the idea with online payments, real-time interaction, and new dynamics.
+        </p>
+
+        <p className="mb-2">
+          My name is Santiago, I’m twenty-three years old, and my brother and I are the main providers for our family. We are a family of five. We used to live in a small house where all five of us shared the same room. Since I got a slightly more stable job, we’ve been able to move into a more comfortable house, but it’s still not ours. I've been working since I was 14 to help my parents and pay part of my school tuition.
         </p>
         <p className="mb-2">
-          I've been trying to bring this project to life for a long time, but I couldn’t because I lacked the necessary resources.
-          I even bought a couple of laptops to work on it, but they were stolen, one night when I invited a friend to my house so he could sleep because he had nowhere to go,
-          he took them and disappeared. So now, I’m here, working on this project in my free time after my regular job.
+          I've been trying to bring this project to life for a long time, but I couldn't because I lacked the necessary resources. I even took out a loan to buy a couple of laptops—one to sell and start a buying-and-selling business to manage some capital and another to work on this project. But they were stolen. One night, I invited a friend to my house so he could have a place to sleep because he had nowhere to go. He took them and disappeared. In the end, I tried to help someone in need, and now I am the one who needs help. So now, I’m here, working on this project in my free time after my regular job.
         </p>
+
         <p className="mb-2">
-          My name is Santiago, I'm in my twenties, and my brother and I are the main providers for our family. I've been working since I was 14 to help my parents.
+          I'm doing this because I want to study. I dream of applying for a scholarship abroad, but I can't because I am the backbone of my family. They need me every day, and we rely on each other. I want to fulfill my mother’s dream of owning a house and taking her to visit Turkey. It hurt me deeply to hear her say the other day that she had given up and would never be able to go.
         </p>
-        <p className="mb-2">
-          I’m doing this because I want to study. I dream of applying for a scholarship abroad, but I can’t because I am the provider for my family and they need me every day—we need each other.
-          So here I am, trying to make it happen.
-        </p>
-        <p className="mb-2">
-          I want to fulfill my mother’s dream of owning a house and taking her to visit Turkey. It hurt me deeply to hear her say the other day that she had given up and would never be able to go.
-        </p>
-        <p className="mb-2">
-          PixelsWar is inspired by the iconic <span className="text-lime-600">Million Dollar Homepage</span> created by Alex Tew in 2005, a groundbreaking project that sold pixels as advertising space.
-          At the time, I was only three years old and couldn't compete with him—but now, with modern technology, I’m bringing the concept back with a twist.
-          Our version upgrades the idea with cryptocurrency payments, real-time interaction, and new dynamics.
-        </p>
+
         <p className="mb-2 font-semibold text-lime-600">
-          I don’t seek fame, influence, or recognition. I just want my family and me to be happy.
+          I don’t seek fame, influence, or recognition. That’s why I won’t share my social media, even though I would like to. I just want my family and me to be happy.
         </p>
+
         <p className="text-white text-center mt-4 cursor-pointer">
           So, could you please help me?
         </p>
