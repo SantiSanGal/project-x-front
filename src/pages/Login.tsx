af4ba737f18132575af6b43d43ce5bebe9f0f26d
Santiago Galvan <sngalvan@tu.com.py>
2025-08-13 17:32:42 -0300

    chore: refactor icons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { EyeClosedIcon } from "@/icons/EyeCloseIcon";
 import { useMutation } from "@tanstack/react-query";
 import { useUserStore } from "@/store/loginStore";
+import { EyeOpenIcon } from "@/icons/EyeOpenIcon";
 import { postLogin } from "@/core/actions/auth";
 import { useNavigate } from "react-router-dom";
 import { millionApi } from "@/api/million.api";
@@ -8,49 +10,15 @@ import { Spinner } from "@/components";
 import { useState } from "react";
 import { toast } from "sonner";
 import {
+  GoogleOAuthProvider,
   CredentialResponse,
   GoogleLogin,
-  GoogleOAuthProvider,
 } from "@react-oauth/google";
 
 interface LoginFormData {
   username: string;
   password: string;
 }
-
-const EyeOpenIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-    <circle cx="12" cy="12" r="3"></circle>
-  </svg>
-);
-
-const EyeClosedIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
-    <line x1="1" y1="1" x2="23" y2="23"></line>
-  </svg>
-);
 
 export const Login = () => {
   const loginAction = useUserStore((state) => state.login);
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,13 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { EyeClosedIcon } from "@/icons/EyeCloseIcon";
 import { useMutation } from "@tanstack/react-query";
 import { useUserStore } from "@/store/loginStore";
+import { EyeOpenIcon } from "@/icons/EyeOpenIcon";
+import { GoogleIcon } from "@/icons/GoogleIcon";
 import { useNavigate } from "react-router-dom";
 import { millionApi } from "@/api/million.api";
 import ReCAPTCHA from "react-google-recaptcha";
+import { Spinner } from "@/icons/Spinner";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -16,81 +20,6 @@ interface RegisterFormData {
   password: string;
   confirm_password: string;
 }
-
-const EyeOpenIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    {" "}
-    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>{" "}
-    <circle cx="12" cy="12" r="3"></circle>{" "}
-  </svg>
-);
-
-const EyeClosedIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    {" "}
-    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>{" "}
-    <line x1="1" y1="1" x2="23" y2="23"></line>{" "}
-  </svg>
-);
-
-const GoogleIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    className="fill-blue-500"
-  >
-    {" "}
-    <path d="M22.56,12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26,1.37-1.04,2.53-2.21,3.31v2.77h3.57c2.08-1.92,3.28-4.74,3.28-8.09Z"></path>{" "}
-    <path d="M12,23c2.97,0,5.46-.98,7.28-2.66l-3.57-2.77c-.98.66-2.23,1.06-3.71,1.06-2.86,0-5.29-1.93-6.16-4.53H2.18v2.84C3.99,20.53,7.7,23,12,23Z"></path>{" "}
-    <path d="M5.84,14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43,8.55,1,10.22,1,12s.43,3.45,1.18,4.93l3.66-2.84Z"></path>{" "}
-    <path d="M12,5.38c1.62,0,3.06.56,4.21,1.64l3.15-3.15C17.45,2.09,14.97,1,12,1,7.7,1,3.99,3.47,2.18,7.07l3.66,2.84c.87-2.6,3.3-4.53,6.16-4.53Z"></path>{" "}
-  </svg>
-);
-
-const Spinner = () => (
-  <svg
-    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <circle
-      className="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      strokeWidth="4"
-    ></circle>
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    ></path>
-  </svg>
-);
 
 declare global {
   interface Window {
@@ -143,10 +72,10 @@ export const Register = () => {
         });
     },
     onSuccess: () => {
-      toast.success("¡Registro exitoso!");
+      toast.success("User Registered Successfully");
       setTimeout(() => {
         navigate("/login");
-      }, 1000);
+      }, 2000);
     },
     onError: (error: any) => {
       const messages = error?.response?.data?.messages || [
@@ -180,7 +109,8 @@ export const Register = () => {
             const expiresAt = token.expiresAt || token.expires_at;
 
             // 1) guardar en Zustand (persist)
-            loginAction(accessToken, expiresAt);
+            // loginAction(accessToken, expiresAt);
+            useUserStore.getState().login(accessToken, expiresAt);
 
             // 2) fijar header para siguientes requests
             millionApi.defaults.headers.common[
